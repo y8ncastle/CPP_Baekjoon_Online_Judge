d33c60bd6271fe0642a6fff95ce952d64d53e09c
Zay (Yoonsung), Jeong <52089296+y8ncastle@users.noreply.github.com>
2020-11-20 14:02:16 +0900

    Update 6603_fail.cpp

--- 6603_fail.cpp
+++ 6603_fail.cpp
@@ -6,23 +6,18 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     
-    int arr[6], S[12], k;
+    int arr[6], S[12] = {0,}, k;
     
     while (1) {
-        cin >> k;
-        
-        if (k == 0) break;
-        
         for (int i=0; i<6; i++)
             arr[i] = i;
         
-        for (int i=0; i<k; i++)
-            S[i] = 0;
+        cin >> k;
         
         for (int i=0; i<k; i++)
             cin >> S[i];
         
-        while (arr[0] <= arr[1]) {
+        while (1) {
             int check = 0;
             
             for (int i=1; i<6; i++) {
@@ -37,7 +32,10 @@ int main() {
                 for (int i=0; i<6; i++)
                     cout << S[arr[i]] << " ";
                 arr[k-2] += 1;
+                cout << '\n';
             }
+            
+            if (arr[0] > arr[1]) break;
         }
     }
     
