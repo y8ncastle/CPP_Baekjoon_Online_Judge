f8fa535ce318ee803d775e3b214f5541275551f3
Zay (Yoonsung), Jeong <52089296+y8ncastle@users.noreply.github.com>
2020-11-20 16:49:42 +0900

    Create 14681.cpp

---
+++ 14681.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    int x, y;
+    
+    cin >> x >> y;
+    
+    if (x > 0 && y > 0)
+        cout << "1";
+    else if (x < 0 && y > 0)
+        cout << "2";
+    else if (x < 0 && y < 0)
+        cout << "3";
+    else
+        cout << "4";
+       
+    return 0;
+}
